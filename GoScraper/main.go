9f754b213789192e62a3bafab8b60ae014b441e7
Kairose <22220030071@mersin.edu.tr>
2024-11-19 15:39:25 +0300

    GoScraperv2

--- GoScraper/main.go
+++ GoScraper/main.go
@@ -12,6 +12,9 @@ import (
 type Data struct {
 	title, bodytext, date string
 }
+type ScrapeCourse struct {
+	Url, Pname, Price string
+}
 
 func trimLeftChar(s string) string {
 	for i := range s {
@@ -26,9 +29,10 @@ func main() {
 
 	var menu int
 	var webdata []Data
+	var scrapeCourse []ScrapeCourse
 
 	fmt.Println("Welcome To Scraper")
-	fmt.Printf("What do you wanna do?\nScrape 'thehackernews'(-1)\nScraping Course(-2)\nYavuzlar(-3)\nExit(-4)")
+	fmt.Printf("What do you wanna do?\nScrape 'thehackernews'(-1)\nScraping Course(-2)\nPagination(-3)\nExit(-4)")
 	fmt.Scanln(&menu)
 
 	switch menu {
@@ -79,21 +83,21 @@ func main() {
 		}
 
 	case -2:
-		fmt.Println("Scraping HackerNews...")
+		fmt.Println("Scraping Scrape Course...")
 		c := colly.NewCollector()
 
-		c.OnHTML(".clear.home-right", func(e *colly.HTMLElement) {
+		c.OnHTML("li.product", func(e *colly.HTMLElement) {
 
-			data := Data{}
-			data.title = "Title:" + e.ChildText((".home-title"))
-			data.bodytext = e.ChildText((".home-desc"))
-			data.date = e.ChildText((".h-datetime"))
-			webdata = append(webdata, data)
+			data := ScrapeCourse{}
+			data.Url = e.ChildAttr("a", "href")
+			data.Pname = e.ChildText(".product-name")
+			data.Price = e.ChildText((".price"))
+			scrapeCourse = append(scrapeCourse, data)
 		})
 
 		c.OnScraped(func(r *colly.Response) {
 
-			file, err := os.Create("hackernews.csv")
+			file, err := os.Create("scrapecourse.csv")
 			if err != nil {
 				log.Fatalln("Failed to create output CSV file", err)
 			}
@@ -101,30 +105,81 @@ func main() {
 			writer := csv.NewWriter(file)
 
 			headers := []string{
-				"Title",
-				"Description",
-				"Date",
+				"Url",
+				"ProductName",
+				"Price",
 			}
 			writer.Write(headers)
 
-			for _, data := range webdata {
+			for _, data := range scrapeCourse {
 
 				record := []string{
-					data.title,
-					data.bodytext,
-					trimLeftChar(data.date),
+					data.Url,
+					data.Pname,
+					data.Price,
 				}
 				writer.Write(record)
 			}
 			defer writer.Flush()
 		})
 
-		err := c.Visit("https://thehackernews.com/")
+		err := c.Visit("https://www.scrapingcourse.com/ecommerce/")
 		if err != nil {
 			fmt.Println("Visit error:", err)
 		}
 
 	case -3:
+
+		fmt.Println("Scraping Scrape Course...")
+		c := colly.NewCollector()
+
+		c.OnHTML(".product-item", func(e *colly.HTMLElement) {
+
+			data := ScrapeCourse{}
+			data.Url = e.ChildAttr("a", "href")
+			data.Pname = e.ChildText(".product-name")
+			data.Price = e.ChildText((".product-price"))
+			scrapeCourse = append(scrapeCourse, data)
+		})
+
+		c.OnHTML("a[rel='next']", func(e *colly.HTMLElement) {
+			nextPage := e.Attr("href")
+			fmt.Println("Navigating to:", nextPage)
+			e.Request.Visit(nextPage)
+		})
+
+		c.OnScraped(func(r *colly.Response) {
+
+			file, err := os.Create("ScrapePagination.csv")
+			if err != nil {
+				log.Fatalln("Failed to create output CSV file", err)
+			}
+			defer file.Close()
+			writer := csv.NewWriter(file)
+
+			headers := []string{
+				"Url",
+				"ProductName",
+				"Price",
+			}
+			writer.Write(headers)
+
+			for _, data := range scrapeCourse {
+
+				record := []string{
+					data.Url,
+					data.Pname,
+					data.Price,
+				}
+				writer.Write(record)
+			}
+			defer writer.Flush()
+		})
+
+		err := c.Visit("https://www.scrapingcourse.com/pagination/")
+		if err != nil {
+			fmt.Println("Visit error:", err)
+		}
 
 	case -4:
 		fmt.Println("Exiting...")
